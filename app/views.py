82a8f9fa2bb9d696f51851408ceb3184d97351ce
manasa maddipatla <manasamaddipatla@gmail.com>
2023-05-25 19:31:36 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from app.models import *
-from django.views.generic import TemplateView,ListView,DetailView,CreateView
+from django.views.generic import TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -22,3 +23,12 @@ class SchoolDetail(DetailView):
 class SchoolCreate(CreateView):
     model=School
     fields='__all__'
+
+class SchoolUpdate(UpdateView):
+    model=School
+    fields='__all__'
+
+class SchoolDelete(DeleteView):
+    model=School
+    context_object_name='schoolobject'
+    success_url=reverse_lazy('SchoolList')
--- project41/urls.py
+++ project41/urls.py
@@ -23,5 +23,9 @@ urlpatterns = [
     path('SchoolList/',SchoolList.as_view(),name='SchoolList'),
     path('SchoolCreate/',SchoolCreate.as_view(),name='SchoolCreate'),
 
+
+    re_path('^Update/(?P<pk>\d+)/',SchoolUpdate.as_view(),name='SchoolUpdate'),
+    re_path('^delete/(?P<pk>\d+)/',SchoolDelete.as_view(),name='SchoolDelete'),
+
     re_path('(?P<pk>\d+)/',SchoolDetail.as_view(),name='SchoolDetail'),
 ]
